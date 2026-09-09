4e889a5f5926662fdb8be6dadfd2532fec37aeb5
srihareeshh <srihareeshh@gmail.com>
2026-09-09 16:40:05 +0530

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-def add(a,b):
-    return a+b
-
-def subract(a,b):
-    return a-b
-        
+def add(a, b):
+return a + b
+def subtract(a, b):
+return a - b
--- test_app.py
+++ test_app.py
@@ -1,8 +1,6 @@
+from app import add, subtract
+
 def test_add():
-    assert add(2,3)==5
-
-def test_subract():
-    assert subract(5,3)==2
-
-
-
+assert add(2, 3) == 5
+def test_subtract():
+assert subtract(5, 3) == 2
